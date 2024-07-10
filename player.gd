5dcd5d89fcaa50d0eb9c0482e362e6f0644107e4
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-07-10 20:24:38 +0200

    voeg camera toe  aan de player scene

--- player.gd
+++ player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
-var double_jump = 0
+const SPEED :float = 300.0
+const JUMP_VELOCITY :float = -600.0
+var double_jump :int = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var coyotejumptimer = $coyotejumptimer
 
 func _physics_process(delta):
+	if velocity.x > 0:
+		$Sprite2D.flip_h = false
+	elif velocity.x < 0:
+		$Sprite2D.flip_h = true
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
