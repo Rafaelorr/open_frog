bd15994fc2fdea2a48e4b3c3a8d87e671bee00c1
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-07-13 14:33:08 +0200

    voeg het begin van de  munt toe

---
+++ munt.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var state = "no_munt" # no_munt, munt
+var player_in_area = false
+
+func _process(delta):
+	if state == "munt":
+		if player_in_area:
+			pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("player"):
+		player_in_area = true
+
+
+func _on_area_2d_body_exited(body):
+	if body.has_method("player"):
+		player_in_area = false
