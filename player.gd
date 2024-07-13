4b0c72cf4f473e6cbd599b935791b2883f3c00c2
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-07-13 14:24:51 +0200

    voeg stop methode toe aan player.gd

--- player.gd
+++ player.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 const SPEED :float = 300.0
 const JUMP_VELOCITY :float = -600.0
 var double_jump :int = 0
+@export var speler_aantal_munten = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var coyotejumptimer = $coyotejumptimer
+@onready var coyotejumptimer = $coyotejumptime
 
 func _physics_process(delta):
 	if velocity.x > 0:
@@ -20,14 +21,13 @@ func _physics_process(delta):
 	if is_on_floor():
 		double_jump = 1
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor() or Input.is_action_just_pressed("ui_accept") and double_jump == 1 :
+	if Input.is_action_just_pressed("omhoog") and is_on_floor() or Input.is_action_just_pressed("omhoog") and double_jump == 1 :
 			if is_on_floor():
 				double_jump = 1
 				velocity.y = JUMP_VELOCITY
 			elif double_jump == 1:
 				double_jump = 0
 				velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -37,3 +37,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if $Sprite2D.global_position[1] > 3400.0:
+		#TODO: voeg game over systeem toe
+		get_tree().quit(3)
+	elif Input.is_action_just_pressed("stop"):
+		get_tree().quit(3)
+
+func player():
+	pass
