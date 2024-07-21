db7702de95629e6beaf74a8b9fe2168ac21bbd78
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-07-21 16:03:43 +0200

    verwijder warnings bij de player scene

--- player.gd
+++ player.gd
@@ -7,7 +7,6 @@ var double_jump :int = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var coyotejumptimer = $coyotejumptime
 
 func _physics_process(delta):
 	if velocity.x > 0:
