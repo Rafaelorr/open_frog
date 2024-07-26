63d38afd055a05e536772b11784a642fd00c2d85
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-07-26 18:06:05 +0200

    voeg werkende munten toe

---
+++ global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var aantal_sleutels :int = 0
+var aantal_munten :int = 0
--- munt.gd
+++ munt.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-var state = "no_munt" # no_munt, munt
+var verzameld = false
 var player_in_area = false
 
 func _process(_delta):
-	if state == "munt":
+	if not verzameld:
 		if player_in_area:
-			pass
+			verzameld = true
+			Global.aantal_munten += 1
+			print(Global.aantal_munten)
+			queue_free()
 
 
 func _on_area_2d_body_entered(body):
