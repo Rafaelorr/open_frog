2731ad046ff5a765a374c2af71f0babf0d5b1ad9
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-07-26 18:01:04 +0200

    voeg een gesloten deur en sleutel item toe

--- door.gd
+++ door.gd
@@ -2,13 +2,19 @@ extends Area2D
 
 var entered :bool = false
 @export var connected_scene_file_path : String
-var scene_folder = "res://"
+@export var Open : bool = true
+var scene_folder = "res://levels/"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if entered:
-		var full_path = scene_folder + connected_scene_file_path + ".tscn"
-		get_tree().change_scene_to_file(full_path)
+		if Open:
+			var full_path = scene_folder + connected_scene_file_path + ".tscn"
+			get_tree().change_scene_to_file(full_path)
+		else:
+			if Global.aantal_sleutels > 0:
+				Open = true
+				Global.aantal_sleutels -= 1
 
 func _on_body_entered(body: CharacterBody2D):
 	if body.has_method("player"):
