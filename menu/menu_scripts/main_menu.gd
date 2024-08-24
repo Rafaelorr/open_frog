53123b38dd671ac668c4db9598df4f20bc7d1716
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-08-24 12:19:51 +0200

    voeg een menu toe die de controlles tonen

--- menu/menu_scripts/main_menu.gd
+++ menu/menu_scripts/main_menu.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://levels/level_scenes/level_1.tscn")
 
 
 func _on_button_2_pressed():
-	print("controlles")
+	get_tree().change_scene_to_file("res://menu/menu_scenes/controlles.tscn")
