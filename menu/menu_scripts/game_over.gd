4f87852e37a5315ae8df7a8904ea3c53ab2752d1
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-08-25 17:49:48 +0200

    verander file path in game_over.gd

--- menu/menu_scripts/game_over.gd
+++ menu/menu_scripts/game_over.gd
@@ -4,5 +4,5 @@ func _on_stop_button_pressed():
 	get_tree().quit(3)
 
 func _on_speel_verder_button_pressed():
-	var change_level :String = "res://levels/level_%d" % Global.huidig_level +".tscn"
+	var change_level :String = "res://levels/level_scenes/level_%d" % Global.huidig_level +".tscn"
 	get_tree().change_scene_to_file(change_level)
