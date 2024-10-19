516037737acad8465462c5bab55607337f66cd66
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-10-19 11:08:37 +0200

    optimize key.gd

--- obstaceles/obstacel_scripts/key.gd
+++ obstaceles/obstacel_scripts/key.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
-var verzameld = false
-
 func _on_body_entered(body):
-	if body.has_method("player") and not verzameld:
-		verzameld = true
+	if body is speler and not verzameld:
 		Global.aantal_sleutels += 1
 		queue_free()
