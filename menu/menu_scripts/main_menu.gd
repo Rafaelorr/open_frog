7b999dbe5df6e094ba1c4c3d423330a6202f611b
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-10-19 11:04:37 +0200

    verwijder dead space in main_menu.gd

--- menu/menu_scripts/main_menu.gd
+++ menu/menu_scripts/main_menu.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
-
-
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://levels/level_scenes/level_1.tscn")
-
 
 func _on_button_2_pressed():
 	get_tree().change_scene_to_file("res://menu/menu_scenes/controlles.tscn")
