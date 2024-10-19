935cc18a4acfd0f24eccc66a16e5b11a5fbf4810
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-10-19 11:11:36 +0200

    Revert "optimize key.gd"
    This reverts commit 516037737acad8465462c5bab55607337f66cd66.

--- obstaceles/obstacel_scripts/key.gd
+++ obstaceles/obstacel_scripts/key.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+var verzameld = false
+
 func _on_body_entered(body):
-	if body is speler and not verzameld:
+	if body.has_method("player") and not verzameld:
+		verzameld = true
 		Global.aantal_sleutels += 1
 		queue_free()
