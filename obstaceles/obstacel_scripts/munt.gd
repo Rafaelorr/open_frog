8c75163b6e7df09fc3a6b0e8aeed5352e5ddf60b
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-10-19 11:25:47 +0200

    verwijder dead space in munt.gd

--- obstaceles/obstacel_scripts/munt.gd
+++ obstaceles/obstacel_scripts/munt.gd
@@ -4,18 +4,14 @@ var verzameld : bool = false
 var player_in_area :bool = false
 
 func _process(_delta):
-	if not verzameld and player_in_area:
+	if not verzameld and player_in_area: 
 		verzameld = true
 		Global.aantal_munten += 1
 		print(Global.aantal_munten)
 		queue_free()
 
-
 func _on_area_2d_body_entered(body):
-	if body is speler:
-		player_in_area = true
-
+	if body is speler: player_in_area = true
 
 func _on_area_2d_body_exited(body):
-	if body is speler:
-		player_in_area = false
+	if body is speler: player_in_area = false
