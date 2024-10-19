574eafd532e245b21c1a970caf86a3a15b2dc8f2
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-10-19 11:19:22 +0200

    optimize munt.gd

--- obstaceles/obstacel_scripts/munt.gd
+++ obstaceles/obstacel_scripts/munt.gd
@@ -1,22 +1,21 @@
 extends Node2D
 
-var verzameld = false
-var player_in_area = false
+var verzameld : bool = false
+var player_in_area :bool = false
 
 func _process(_delta):
-	if not verzameld:
-		if player_in_area:
-			verzameld = true
-			Global.aantal_munten += 1
-			print(Global.aantal_munten)
-			queue_free()
+	if not verzameld and player_in_area:
+		verzameld = true
+		Global.aantal_munten += 1
+		print(Global.aantal_munten)
+		queue_free()
 
 
 func _on_area_2d_body_entered(body):
-	if body.has_method("player"):
+	if body is speler:
 		player_in_area = true
 
 
 func _on_area_2d_body_exited(body):
-	if body.has_method("player"):
+	if body is speler:
 		player_in_area = false
