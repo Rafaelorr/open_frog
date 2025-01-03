549e09f190530354d63e5168b5cb508bc2312f89
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-01-03 09:10:04 +0100

    voeg een wait functie toe aan global.gd

--- global.gd
+++ global.gd
@@ -2,5 +2,8 @@ extends Node
 
 var aantal_sleutels :int = 0
 var aantal_munten :int = 0
-
 var huidig_level :int = 0
+var level_tijden :Array[float] = [0.0,0.0,0.0,0.0,0.0,0.0]
+
+func wait(seconds: float) -> void:
+  await get_tree().create_timer(seconds).timeout
